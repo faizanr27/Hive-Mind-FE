e2649f6f2870cc1bb9cae4fce67ab93056f99714
Faizan <faizanms025347@gmail.com>
2025-01-28 01:49:33 +0530

    added bg-gradient

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -19,8 +19,8 @@ export function Dashboard() {
   }
 
   return (
-    <div className="absolute inset-0 min-h-screen dotted-background overflow-x-hidden">
-      <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-neutral-950/90 to-transparent">
+    <div className="absolute inset-0 min-h-screen full-page-gradient dotted-background overflow-x-hidden">
+      <div className="absolute inset-0 ">
         <div className="absolute bottom-56 right-[200px] w-36 h-[1000px] rotate-45">
           <div className="w-full h-full bg-gradient-to-t to-slate-600/20 via-slate-600/30 from-transparent rounded-tl-[800px] rounded-tr-[800px] blur-2xl" />
         </div>
