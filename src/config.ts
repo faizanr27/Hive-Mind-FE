3d9fff7bdb7eca7f0198d5d93653700860332459
Faizan Raza <faizanms025347@gmail.com>
2025-03-11 14:06:55 +0530

    updated backend url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://hive-mind.up.railway.app/api"
+export const BACKEND_URL = "https://hive.shortsy.xyz/api"
 // export const BACKEND_URL = "http://localhost:5000/api"
